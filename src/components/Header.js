878f785b1e54f984750b0c054d7d0e6ac6652d68
Ilya <ilya@sinyugin.ru>
2022-01-09 23:01:32 +0100

    button added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,13 @@
 import PropTypes from 'prop-types'
-
+import Button from './Button'
 const Header = ({ title }) => {
+    const onClick = () => {
+        console.log('Header clicked')
+    }
     return (
-        <header>
+        <header className='header'>
             <h1>{title}</h1>
+            <Button color='green' text='Add' onClick={onClick} />
         </header>
     )
 }
