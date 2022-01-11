bdd09f8c948f30cce4913902b1059518c8f0898a
Ilya <ilya@sinyugin.ru>
2022-01-11 23:07:37 +0100

    added reminder togle

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import Task from './Task'
 
-const Tasks = ({ tasks, onDelete }) => {
+const Tasks = ({ tasks, onDelete, onToggle }) => {
     return (
         <>
             {tasks.map((task) => (
-                <Task key={task.id} task={task} onDelete={onDelete} />
+                <Task key={task.id} task={task}
+                    onDelete={onDelete} onToggle={onToggle} />
             ))}
         </>
     )
